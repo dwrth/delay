f0442f07eb1d546d126e396db53160b3598febf0
David <75678655+David-Werth@users.noreply.github.com>
2024-12-19 21:40:09 +0100

    Add rotary slider for gain

--- Source/Parameters.h
+++ Source/Parameters.h
@@ -44,4 +44,6 @@ private:
     juce::AudioParameterFloat* delayTimeParam;
     float targetDelayTime = 0.0f;
     float coeff = 0.0f;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Parameters)
 };
--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,9 +13,17 @@
 DelayAudioProcessorEditor::DelayAudioProcessorEditor (DelayAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    slider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
+    slider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 70, 16);
+    slider.setBounds(0, 0, 70, 86);
+    addAndMakeVisible(slider);
+    
+    label.setText("Gain", juce::NotificationType::dontSendNotification);
+    label.setJustificationType(juce::Justification::horizontallyCentred);
+    label.setBorderSize(juce::BorderSize<int>{ 0, 0, 2, 0 });
+    label.attachToComponent(&slider, false);
+    addAndMakeVisible(label);
+    setSize (500, 330);
 }
 
 DelayAudioProcessorEditor::~DelayAudioProcessorEditor()
@@ -25,14 +33,10 @@ DelayAudioProcessorEditor::~DelayAudioProcessorEditor()
 //==============================================================================
 void DelayAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    g.fillAll (juce::Colours::blue);
-    g.setColour (juce::Colours::white);
-    g.setFont (20.0f);
-    g.drawFittedText ("Moin Moin!", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll(juce::Colours::darkgrey);
 }
 
 void DelayAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    slider.setTopLeftPosition(215, 120);
 }
