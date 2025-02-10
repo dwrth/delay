604287e4a969cccc6d48ca35de4a5be1b7dc3bb8
David <75678655+David-Werth@users.noreply.github.com>
2025-02-10 21:08:54 +0100

    Finalize build scripts

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Create build directory if it doesn't exist
+mkdir -p XCodeBuild
+cmake -B XCodeBuild -G Xcode
+
 mkdir -p build
-
-# Generate build files with compile_commands.json
-cmake -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-
-# Create symlink to compile_commands.json in root directory
-ln -sf build/compile_commands.json .
-
-# Build the project
+cmake -B build
 cmake --build build
